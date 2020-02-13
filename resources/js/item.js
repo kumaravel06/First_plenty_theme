8cd16ce496f1417de0afa04262e1c5f4b3632063
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-02-13 17:52:13 +0530

    Update item.js

--- resources/js/item.js
+++ resources/js/item.js
@@ -1,4 +1,5 @@
-$( document ).ready(function() {
+var $ = jQuery.noConflict();
+$(document).ready( function() {
    $('.custom-select').click(function(){
     console.log('testttttt');
      $('option').css('color','#069399');
