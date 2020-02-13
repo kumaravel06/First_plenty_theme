b91ec37630ba9b75ccc10d5a10d95ddef168feae
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-02-13 19:06:42 +0530

    Update item.js

--- resources/js/item.js
+++ resources/js/item.js
@@ -1,7 +1,10 @@
-$(document).ready( function() {
-   $('.custom-select').click(function(){
+window.load(function () {
+    $(document).ready( function() {
+   
+    $('.custom-select').click(function(){
     console.log('testttttt');
      $('option').css('color','#069399');
       console.log('Newwww');
   });
+});
 });
