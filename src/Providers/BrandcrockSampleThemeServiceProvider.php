f417d5f4aff7dae1f04f614636190cde01acbe21
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-02-18 16:51:25 +0530

    Update BrandcrockSampleThemeServiceProvider.php

--- src/Providers/BrandcrockSampleThemeServiceProvider.php
+++ src/Providers/BrandcrockSampleThemeServiceProvider.php
@@ -21,16 +21,17 @@ class BrandcrockSampleThemeServiceProvider extends ServiceProvider
          
 	}
 	
-	public function boot(Twig $twig, Dispatcher $eventDispatcher)
-    {
+	public function boot(Twig $twig, Dispatcher $eventDispatcher, ConfigRepository $config)
+    	{
+		
 	$enabledOverrides = explode(", ", $config->get("BrandcrockSampleTheme.templates.override"));
         $eventDispatcher->listen('IO.init.templates', function(Partial $partial)
         {
 	  $partial->set('footer', 'Ceres::PageDesign.Partials.Footer');
 	  if (in_array("footer", $enabledOverrides) || in_array("all", $enabledOverrides))
             {
-                $partial->set('footer', 'CeresVanilla::PageDesign.Partials.Footer');
-		 $this->getLogger(__METHOD__)->error('IFF', 'Loaded');
+                $partial->set('footer', 'BrandcrockSampleTheme::PageDesign.Partials.Footer');
+		 $this->getLogger(__METHOD__)->error('IFF', $config->get("BrandcrockSampleTheme.templates.override"));
             }
         },0);
 	return false;
