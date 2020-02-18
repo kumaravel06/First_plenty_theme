322ff2abf3577f13ef32ea1396fddea0811e356e
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-02-18 16:41:12 +0530

    Update BrandcrockSampleThemeServiceProvider.php

--- src/Providers/BrandcrockSampleThemeServiceProvider.php
+++ src/Providers/BrandcrockSampleThemeServiceProvider.php
@@ -7,6 +7,7 @@ use IO\Extensions\Functions\Partial;
 use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\Templates\Twig;
 use Plenty\Plugin\Log\Loggable;
+use Plenty\Plugin\ConfigRepository;
  
 class BrandcrockSampleThemeServiceProvider extends ServiceProvider
 {
@@ -17,15 +18,20 @@ class BrandcrockSampleThemeServiceProvider extends ServiceProvider
 	 */
 	public function register()
 	{
-         $this->getLogger(__METHOD__)->error('PPPPPPPP', 'NNNNNNN');
+         
 	}
 	
 	public function boot(Twig $twig, Dispatcher $eventDispatcher)
     {
+	$enabledOverrides = explode(", ", $config->get("BrandcrockSampleTheme.templates.override"));
         $eventDispatcher->listen('IO.init.templates', function(Partial $partial)
         {
-	  $partial->set('footer', 'BrandcrockSampleTheme::content.BrandcrockSampleThemeFooter');
-		 $this->getLogger(__METHOD__)->error('Calling', 'RRRRRRRR');
+	  $partial->set('footer', 'Ceres::PageDesign.Partials.Footer');
+	  if (in_array("footer", $enabledOverrides) || in_array("all", $enabledOverrides))
+            {
+                $partial->set('footer', 'CeresVanilla::PageDesign.Partials.Footer');
+		 $this->getLogger(__METHOD__)->error('IFF', 'Loaded');
+            }
         },0);
 	return false;
     }
