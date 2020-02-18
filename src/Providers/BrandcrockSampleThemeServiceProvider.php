bafc74972c7f16b1c8b152a9681081de0ed5eea9
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-02-18 12:39:57 +0530

    Update BrandcrockSampleThemeServiceProvider.php

--- src/Providers/BrandcrockSampleThemeServiceProvider.php
+++ src/Providers/BrandcrockSampleThemeServiceProvider.php
@@ -6,10 +6,11 @@ use Plenty\Plugin\ServiceProvider;
 use IO\Extensions\Functions\Partial;
 use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\Templates\Twig;
+use Plenty\Plugin\Log\Loggable;
  
 class BrandcrockSampleThemeServiceProvider extends ServiceProvider
 {
- 
+ use Loggable;
 	/**
 	 * Register the service provider.
 	 */
@@ -21,7 +22,9 @@ class BrandcrockSampleThemeServiceProvider extends ServiceProvider
     {
         $eventDispatcher->listen('IO.init.templates', function(Partial $partial)
         {
+	  $this->getLoggr(__METHOD__)->error('test', 'enter');
 	  $partial->set('footer', 'BrandcrockSampleTheme::content.BrandcrockSampleThemeFooter');
+		 $this->getLoggr(__METHOD__)->error('No test', 'No enter');
           $partial->set('header', 'BrandcrockSampleTheme::PageDesign.Partials.Header.Header');
 	  
 	  
