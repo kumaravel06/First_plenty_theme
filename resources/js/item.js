2f8b9862c103a6458f00a8706dc9a60706c43eeb
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-02-21 12:16:40 +0530

    Update item.js

--- resources/js/item.js
+++ resources/js/item.js
@@ -3,10 +3,8 @@ window.addEventListener('load', function() {
     $(document).ready( function() {
    
     $('.custom-select').click(function(){
-    console.log('testttttt');
      $('option').css('color','#069399');
-      console.log('Newwww');
   });
-    $('.footer').prepend('<div class="contact"><img src="{{plugin_path('BrandcrockSampleTheme')}}/images/contact.png" alt="contact"><h1>Contact Us</h1></div>');
+    $('.footer').prepend('<div class="contact"><img src="../images/contact.png" alt="contact"><h1>Contact Us</h1></div>');
 });
 });
