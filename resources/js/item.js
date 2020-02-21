125534e2e0398fb2e8859348a4f8b392a10ecdac
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-02-21 17:52:04 +0530

    Update item.js

--- resources/js/item.js
+++ resources/js/item.js
@@ -8,7 +8,7 @@ window.addEventListener('load', function() {
     var bc_contact = $('.bc_contact').clone();
     $('.footer').prepend(bc_contact);
     $('.copyright').prepend('<div class="social-icons"><a href="" class="fa fa-facebook" target="_blank"></a><a href="" class="fa fa-twitter" target="_blank"></a><a href="" class="fa fa-instagram" target="_blank"></a><a href="" class="fa fa-google-plus" target="_blank"></a></div>');   
-    $(#mainNavbarCollapsable ul.mainmenu).click(function(){
+    $('#mainNavbarCollapsable ul.mainmenu').click(function(){
     $('#mainNavbarCollapsable ul.mainmenu').css({
        'background-color':'#fbadc0',
        'background-image':'linear-gradient(to bottom, #fbadc0 0%, #f47795 100%)',
