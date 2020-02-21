fa9b614fea7c55cf7941fd0990823d91bebb405f
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-02-21 14:42:40 +0530

    Update item.js

--- resources/js/item.js
+++ resources/js/item.js
@@ -5,7 +5,10 @@ window.addEventListener('load', function() {
     $('.custom-select').click(function(){
      $('option').css('color','#069399');
   });
-    $('.footer').prepend('<div class="contact"><img src="../images/contact.png" alt="contact"><h1>Contact Us</h1></div>');
+    var bc_contact = $('.bc_contact').clone();
+    $('.bc_contact').remove();
+    $('.footer').prepend(bc_contact);
+    
     $('.copyright').prepend('<div class="social-icons"><a href="" class="fa fa-facebook" target="_blank"></a><a href="" class="fa fa-twitter" target="_blank"></a><a href="" class="fa fa-instagram" target="_blank"></a><a href="" class="fa fa-google-plus" target="_blank"></a></div>');   
     
 });
