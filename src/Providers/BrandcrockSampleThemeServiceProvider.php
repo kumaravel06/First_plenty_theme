b21614c816beed21580e0927c1a0ab05ca42f3fb
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-03-16 11:22:21 +0530

    Update BrandcrockSampleThemeServiceProvider.php

--- src/Providers/BrandcrockSampleThemeServiceProvider.php
+++ src/Providers/BrandcrockSampleThemeServiceProvider.php
@@ -24,8 +24,10 @@ class BrandcrockSampleThemeServiceProvider extends ServiceProvider
 	}
 	
 	public function boot(Twig $twig, Dispatcher $dispatcher) {
-	  $dispatcher->listen('IO.init.templates', function (Partial $partial)
+	  $dispatcher->listen('IO.init.templates', function (Partial $partial) {
 	  $partial->set('header', 'BrandcrockSampleTheme::PageDesign.Partials.Header.Header');
+	}, 0);
+	return false;
 	}
 	
 	
